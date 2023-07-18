6f7809c19621883cb0d970ddbeafe72b111805ac
Dmtitry Krasnov <dkrasnov.dev@gmail.com>
2023-07-18 19:28:10 +0700

    (feat): add details mixin
    common fields (display_name, description) among all schemas

---
+++ internal/ent/mixin/details.go
@@ -0,0 +1,20 @@
+package mixin
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+	"entgo.io/ent/schema/mixin"
+)
+
+type DetailsMixin struct {
+	mixin.Schema
+}
+
+func (DetailsMixin) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("display_name").
+			Optional(),
+		field.String("description").
+			Optional(),
+	}
+}
