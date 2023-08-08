cc0de91c750b8ef4fa432b8e0b516858789d5702
Dmtitry Krasnov <dkrasnov.dev@gmail.com>
2023-08-08 16:50:11 +0700

    (feat): gql introspection restriction for non-admin users

--- internal/http/ent/gql.go
+++ internal/http/ent/gql.go
@@ -1,9 +1,13 @@
 package ent
 
 import (
+	"context"
+
 	"entgo.io/contrib/entgql"
+	"github.com/99designs/gqlgen/graphql"
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/dkrasnovdev/siberiana-api/ent"
+	"github.com/dkrasnovdev/siberiana-api/internal/ent/privacy"
 	"github.com/dkrasnovdev/siberiana-api/internal/gql/resolver"
 )
 
@@ -11,6 +15,25 @@ import (
 func NewGraphQLServer(client *ent.Client) *handler.Server {
 	// Create a new GraphQL server using gqlgen's default server configuration.
 	server := handler.NewDefaultServer(resolver.NewSchema(client))
+
+	// Add middleware to customize GraphQL operation behavior.
+	server.AroundOperations(func(ctx context.Context, next graphql.OperationHandler) graphql.ResponseHandler {
+		// Get the privacy view from the context.
+		view := privacy.FromContext(ctx)
+		
+		// If the view is not provided, disable introspection to prevent schema introspection.
+		if view == nil {
+			graphql.GetOperationContext(ctx).DisableIntrospection = true
+		}
+
+		// If the user is not an administrator, disable introspection to restrict access.
+		if !view.IsAdministrator() {
+			graphql.GetOperationContext(ctx).DisableIntrospection = true
+		}
+
+		// Continue processing the GraphQL operation.
+		return next(ctx)
+	})
 
 	// Use the entgql.Transactioner middleware to automatically handle transactions for each GraphQL operation.
 	server.Use(entgql.Transactioner{TxOpener: client})
